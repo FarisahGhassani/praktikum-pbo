a1c8be97d9b91a8807ed82061fcd1565670e090a
FarisahGhassani <161298508+FarisahGhassani@users.noreply.github.com>
2024-03-26 01:15:59 +0700

    Create Buku.java

---
+++ Praktikum 4 - Tugas tambahan/Buku.java
@@ -0,0 +1,73 @@
+/*  
+*   Nama      = Farisah Ghassani Hasyyati
+*   NIM       = 24060122140137
+*   Nama File = Buku.java 
+*   Deskripsi = Program class Buku
+*/
+
+package bk.buku;
+
+public class Buku {
+
+    protected String judul;
+    protected String penulis;
+    protected String tahunTerbit;
+    protected int jumlahHalaman;
+    protected double harga;
+
+    public Buku(String judul, String penulis, String tahunTerbit, int jumlahHalaman, double harga) {
+        this.judul = judul;
+        this.penulis = penulis;
+        this.tahunTerbit = tahunTerbit;
+        this.jumlahHalaman = jumlahHalaman;
+        this.harga = harga;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public String getPenulis() {
+        return penulis;
+    }
+
+    public void setPenulis(String penulis) {
+        this.penulis = penulis;
+    }
+
+    public String getTahunTerbit() {
+        return tahunTerbit;
+    }
+
+    public void setTahunTerbit(String tahunTerbit) {
+        this.tahunTerbit = tahunTerbit;
+    }
+
+    public int getJumlahHalaman() {
+        return jumlahHalaman;
+    }
+
+    public void setJumlahHalaman(int jumlahHalaman) {
+        this.jumlahHalaman = jumlahHalaman;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+    public void view() {
+        System.out.println("Judul: " + judul);
+        System.out.println("Penulis: " + penulis);
+        System.out.println("Tahun Terbit: " + tahunTerbit);
+        System.out.println("Jumlah Halaman: " + jumlahHalaman);
+        System.out.println("Harga: " + harga);
+    }
+}
